c47357bd57c1177237cbcc92279cfacd634c248c
Krishnakant chavan <70923432+Krishnakant007@users.noreply.github.com>
2024-11-08 12:12:01 +0530

    Update GardenProduct.jsx

--- src/screens/GardenProduct.jsx
+++ src/screens/GardenProduct.jsx
@@ -490,7 +490,7 @@ const UPVCProductScreen = () => {
         ← Back
       </button>
       
-      <h2 className="text-4xl font-bold text-center mb-10 text-gray-800">Garden Pipes</h2>
+      <h2 className="text-4xl font-bold text-center mb-10 text-gray-800 mt-16 ">Garden Pipes</h2>
       <p className="text-center mb-12 text-lg text-gray-600">Explore our range of UPVC pipes:</p>
 
       <div className="grid gap-8 max-w-4xl mx-auto">
